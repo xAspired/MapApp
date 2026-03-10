9b20fff9bf19b1c6f5ea03454ed4d53b02c3fd6b
xAspired <pinoeninfa@libero.it>
2026-03-10 16:58:20 +0100

    changed main.js

--- electron/main.js
+++ electron/main.js
@@ -1,33 +1,33 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const { pathToFileURL } = require("url");
 
 async function createWindow() {
-  // Avvia il server dinamicamente con percorso assoluto
-  const serverPath = path.join(__dirname, "../backend/src/server.js");
-  await import(serverPath);
+  try {
+    const serverPath = path.join(__dirname, "../backend/src/server.js");
+    await import(pathToFileURL(serverPath).href);
 
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
+    const win = new BrowserWindow({
+      width: 1200,
+      height: 800,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    });
 
-  // Carica l'app dal server locale
-  win.loadURL("http://localhost:3000");
+    win.loadURL("http://localhost:3000");
+  } catch (err) {
+    console.error("Errore avviando il server:", err);
+  }
 }
 
-// Crea finestra all'avvio
 app.whenReady().then(createWindow);
 
-// Chiudi app quando tutte le finestre sono chiuse
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
-// Riattiva finestra se su macOS e non ci sono finestre aperte
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
