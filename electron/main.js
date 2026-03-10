963ef18aef70512ad0f8a55d04756dfbdfab5c2c
xAspired <pinoeninfa@libero.it>
2026-03-10 16:49:13 +0100

    changed main.js

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,10 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 async function createWindow() {
-  // Avvia il server dinamicamente
-  await import("../backend/src/server.js");
+  // Avvia il server dinamicamente con percorso assoluto
+  const serverPath = path.join(__dirname, "../backend/src/server.js");
+  await import(serverPath);
 
   const win = new BrowserWindow({
     width: 1200,
@@ -13,15 +15,19 @@ async function createWindow() {
     },
   });
 
+  // Carica l'app dal server locale
   win.loadURL("http://localhost:3000");
 }
 
+// Crea finestra all'avvio
 app.whenReady().then(createWindow);
 
+// Chiudi app quando tutte le finestre sono chiuse
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
+// Riattiva finestra se su macOS e non ci sono finestre aperte
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
